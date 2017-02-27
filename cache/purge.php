c961d1667ad471e6399d6bccf5da6d3d3bd020d0
Mitesh Shah <Mr.Miteshah@gmail.com>
2017-02-27 21:22:18 +0530

    Purge All Cache

---
+++ cache/purge.php
@@ -0,0 +1,11 @@
+<?php
+function removeDirectory($path) {
+        $files = glob($path . '/*');
+        foreach ($files as $file) {
+                is_dir($file) ? removeDirectory($file) : unlink($file);
+        }
+        rmdir($path);
+        return;
+}
+removeDirectory("/run/nginx-cache/");
+?>
